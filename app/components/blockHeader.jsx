a438fcffc0acdd2d1f44b8cd42b9922bfa65e438
bilal-fazlani <bilal.m.fazlani@gmail.com>
2017-01-17 01:43:42 +0530

    added blur and gray color to headerblock when loading

--- app/components/blockHeader.jsx
+++ app/components/blockHeader.jsx
@@ -6,28 +6,33 @@ import {removeHandle} from '../actionsCreators/handleActions'
 
 class BlockHeader extends React.Component {
     render() {
+
+        let headerClassNames = `${this.props.tweetsLoading?"gray":""} ${this.props.isSaving?"blur":""}`
+
         return <div style={{display: "flex",
-                justifyContent: "space-between",
-                position: 'relative'}}>
-                <h2 className={this.props.isSaving?"saving":""} style={{display: "inline", marginTop: 10}}>#{this.props.name}</h2>
-                {this.props.tweetsLoading === true ?
-                    <RefreshIndicator
-                        size={40}
-                        left={0}
-                        top={0}
-                        loadingColor="#FF9800"
-                        status="loading"
-                        style={{position: 'relative',
-                            display: 'inline-block',
-                            margin:7}}
-                    />
-                    :
-                    <IconButton style={{verticalAlign: "middle"}}
-                                onClick={()=>this.props.onRemoveClick(this.props.id)}
-                                tooltip={`Remove #${this.props.name}`}>
-                        <ActionRemove />
-                    </IconButton>}
-            </div>
+                    justifyContent: "space-between",
+                    position: 'relative'}}>
+                    <h2 className={headerClassNames} style={{display: "inline", marginTop: 10}}>#{this.props.name}</h2>
+                    {this.props.isSaving === true ?
+                        <RefreshIndicator
+                            size={40}
+                            left={0}
+                            top={0}
+                            loadingColor="#FF9800"
+                            status="loading"
+                            style={{position: 'relative',
+                                display: 'inline-block',
+                                margin:7}}
+                        />
+                        :
+                        <IconButton style={{verticalAlign: "middle"}}
+                                    onClick={()=>this.props.onRemoveClick(this.props.id)}
+                                    tooltip={`Remove #${this.props.name}`} tooltipPosition="top-center"
+                                    >
+                            <ActionRemove />
+                        </IconButton>}
+
+                </div>
     }
 }
 
